90f9116cfb611f363c68580397b99f7d55a95471
Aurelio Floris <aureliofloris6@gmail.com>
2026-02-20 14:23:42 +0100

    added little things

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 _This project (philosophers) has been created as part of the 42 curriculum by afloris._
 
 # PHILOSOPHERS: a project about multithreading
-## Description
 
+## Description
+Note for self: it's like they're sitting in a circle, philo1 sits next to philoN,
+
+### arguments
+Args will be:
+av[1] = number of philosophers and forks
+av[2] = *time_to_die*  (milliseconds) -> if a philosopher has not started eating within this time since their
+last meal or start of the simulation, they die.
+av[3] = *time_to_eat* (milliseconds) -> the time it takes a philosopher to eat. While they eat, they hold TWO forks.
+av[4] = *time_to_sleep* (milliseconds) -> the time a philosopher spends sleeping.
+There is a last, optional argument:
+av[5] = *number_of_times_each_philosopher_must_eat* -> if all philosophers have eaten at least as much as n, the simulation stops. 
+If this last argument is not included, the simulation stops when the philosopher dies.
 
 ## Instructions
 
--- philo/main.c
+++ philo/main.c
@@ -1,33 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/19 16:24:30 by babyf             #+#    #+#             */
-/*   Updated: 2026/02/20 12:52:03 by babyf            ###   ########.fr       */
+/*   Created: 2026/02/19 17:23:13 by babyf             #+#    #+#             */
+/*   Updated: 2026/02/20 12:57:59 by babyf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-#define PHILO_H
+/* momentary file */
+#include "philo.h"
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/time.h>
-#include <pthread.h>
-
-/* parsing */
-
-/* eat */
-
-/* think */
-
-/* go to sleep */
-
-
-
-#endif
+int		main(int ac, char **av)
+{
+	if (ac != 6) /* or 5 without the optional argument */
+		return (0); /* or error message */
+}
